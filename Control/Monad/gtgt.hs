e69d4829d6b4dabd304b7decdff99c81e6273f51
aiya000 <aiya000.develop@gmail.com>
2016-09-12 12:44:44 +0900

    Learn (>>)

--- Control/Monad/gtgt.hs
+++ Control/Monad/gtgt.hs
@@ -1,11 +1,9 @@
+firstLeftOrLastRight :: [Either e a] -> Either e a
+firstLeftOrLastRight = foldr1 (>>)
+
 main :: IO ()
 main = do
-  print $ Nothing    >> Just 10
-  print $ Left "aho" >> Right 10
   let rights   = [Right 1, Right 2, Right 3, Right 4] :: [Either String Int]
   let someLeft = [Right 1, Right 2, Left "oops!", Right 4]
   print $ foldr1 (>>) rights
   print $ foldr1 (>>) someLeft
-
-firstLeftOrLastRight :: [Either e a] -> Either e a
-firstLeftOrLastRight = foldr1 (>>)
