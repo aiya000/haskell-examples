da71fccadb8b971cf279c917ac5e2dac7b4af5d8
aiya000 <aiya000.develop@gmail.com>
2017-06-12 00:46:47 +0900

    Use EitherT instead of ExceptT

--- Control/Monad/Morph/MMonad.hs
+++ Control/Monad/Morph/MMonad.hs
@@ -1,20 +1,38 @@
-import Control.Exception.Safe (MonadCatch, try, SomeException)
+import Control.Exception.Safe (MonadCatch, try, SomeException, throwM, Exception)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Morph (embed)
+import Control.Monad.Morph (MMonad, embed)
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Except (ExceptT(..), runExceptT)
+import Control.Monad.Trans.Either (EitherT(..), runEitherT)
+
+data AnException = AnException String
+  deriving (Show)
+
+instance Exception AnException
 
 
-check :: IO a -> ExceptT SomeException IO a
-check = ExceptT . try
+instance MMonad (EitherT e) where
+  embed f m = EitherT $ do
+    x <- runEitherT . f $ runEitherT m
+    return $ case x of
+      Left e          -> Left e
+      Right (Left e') -> Left e'
+      Right (Right a) -> Right a
+
+
+check :: IO a -> EitherT SomeException IO a
+check = EitherT . try
 
 program :: (MonadCatch m, MonadIO m) => m String
-program = liftIO $ readFile ""
+program = liftIO . throwM $ AnException "nico"
 
 
 main :: IO ()
 main = do
-  xOrErr <- runExceptT $ embed check program
+  xOrErr <- runEitherT $ embed check program
   case xOrErr of
     Right x -> putStrLn $ "Success: " ++ x
     Left  y -> putStrLn $ "Left value is got !: " ++ show y
+
+
+-- output
+-- Left value is got !: AnException "nico"
