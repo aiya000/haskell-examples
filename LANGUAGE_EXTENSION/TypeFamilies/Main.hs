f056e29a77fd840bb477d4a54cb38b1d75529069
aiya000 <aiya000.develop@gmail.com>
2017-06-21 00:45:00 +0900

    Refine TypeFamilies/Main.hs

--- LANGUAGE_EXTENSION/TypeFamilies/Main.hs
+++ LANGUAGE_EXTENSION/TypeFamilies/Main.hs
@@ -1,58 +1,33 @@
--- See http://qiita.com/CyLomw/items/7e72e98cfe1c8026df2a
 {-# LANGUAGE TypeFamilies #-}
 
--- Simply -- {{{
+-- Func is a type level function (a type function).
+-- * is kind, and it is a type level type.
+type family Func a :: *
+type instance Func Int  = Int
+type instance Func Bool = Int
 
-type family Partial t :: *
-type instance Partial Int    = Double
-type instance Partial String = Int
+type X = Func Int  -- ==> Int
+type Y = Func Bool -- ==> Int
+type Z = Func Char -- ==> Z
 
-x1 :: Double
-x1 = 1.0
+-- :kind! X
+-- :kind! Y
+-- :kind! Z
 
-x2 :: Partial Int
-x2 = 2.0
+-- I don't know how do Z will be defined xD
+-- I think Z has no value.
 
-x3 :: Partial Char
-x3 = undefined
 
--- Compile error
---x4 :: Partial Char
---x4 = 3.0
+-- The type function can be defined as a partial function.
+type family Func' a :: *
+type instance Func' Int = Int
+--type instance Func' Char = Char
+type instance Func' a = Int
 
-x5 :: Partial String
-x5 = 10
+-- `Func' Char` cannot be defined,
+-- because the type function must save a constraint.
+-- `Func' Char` and `Func' a` are conflicted x(
 
-f :: IO ()
-f = do
-  print x1
-  print x2
-  --print x3
-  print x5
-
--- }}}
-
--- Polymorphic -- {{{
-
-type family Poly t :: *
-type instance Poly a = String
-
-y1 :: Poly Double
-y1 = "1.0"
-
-y2 :: Poly Char
-y2 = "a"
-
-y3 :: Poly [Int]
-y3 = "[1, 2, 3]"
-
-g :: IO ()
-g = do
-  print y1
-  print y2
-  print y3
-
--- }}}
 
 main :: IO ()
-main = f >> g
+main = return ()
