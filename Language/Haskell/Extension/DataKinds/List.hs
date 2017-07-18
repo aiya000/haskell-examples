f8aa594fee0d4cbaa7a66f763718462b8b1596b1
aiya000 <aiya000.develop@gmail.com>
2017-07-18 18:48:08 +0900

    Learn a recursive function of type level

--- Language/Haskell/Extension/DataKinds/List.hs
+++ Language/Haskell/Extension/DataKinds/List.hs
@@ -1,4 +1,10 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
+import Data.Void (Void)
+
 
 type X = '[True, False]
 
@@ -6,3 +12,18 @@ type X = '[True, False]
 --
 -- X :: [Bool]
 -- = X
+
+type Y = '[Int]
+-- Y :: [*]
+
+type Z = '(Int, Int)
+
+
+-- | Add a type and a type
+data a :+: b = Refl
+
+type family Sum (a :: [*]) :: * where
+  Sum '[] = Void
+  Sum (x:xs) = x :+: Sum xs
+
+type A = Sum '[Int, Bool, Char]
