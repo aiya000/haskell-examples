047ec9655ce463a143a3891133b2aaae150df2d2
aiya000 <aiya000.develop@gmail.com>
2017-12-08 22:47:29 +0900

    Improve the example of FlexibleContexts

--- Language/Haskell/Extension/FlexibleContexts/Main.hs
+++ Language/Haskell/Extension/FlexibleContexts/Main.hs
@@ -1,26 +1,10 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 
-class LimitedConst a b where
-  limitedConst :: a -> b -> a
-  limitedConst = const
+import GHC.Prim (coerce)
+import GHC.Types (Coercible)
 
-instance LimitedConst Int Int
-instance LimitedConst Char Int
-
-
-x :: Int
-x = 10
-
-y :: Int
-y = 20
-
--- FlexibleContexts
-id' :: (LimitedConst a Int) => a -> a
-id' = flip limitedConst $ x
+toInt :: Coercible a Int => a -> Int
+toInt = coerce
 
 main :: IO ()
-main = do
-  print $ id' y
-  print $ id' 'x'
-  --print $ id' "ahoge"  -- No instance for (limitedConst String Int)
+main = return ()
