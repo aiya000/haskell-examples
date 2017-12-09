08bc5cb7386bb1f54deb27b582cb598075ff9033
aiya000 <aiya000.develop@gmail.com>
2017-12-09 20:27:45 +0900

    Learn RebindableSyntax

---
+++ Language/Haskell/Extension/RebindableSyntax/Main.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE RebindableSyntax #-}
+
+-- RebindableSyntax turns on NoImplicitPrelude
+import Prelude hiding (fromInteger)
+
+-- In the Haskell report specification,
+-- a literal `1` is expanded to `Prelude.fromInteger (1 :: Integer)`.
+--
+-- RebindableSyntax changes it to that is expanded to `fromInteger (1 :: Integer)`,
+-- also it means `1`'s convertion is able to rebind.
+fromInteger :: Integer -> String
+fromInteger _ = ";P"
+
+main :: IO ()
+main = putStrLn 1
