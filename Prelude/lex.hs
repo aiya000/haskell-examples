007a90d2905639f5e0fa78a9361947ab30d5fa84
aiya000 <aiya000.develop@gmail.com>
2018-04-17 23:25:28 +0900

    :thinking: Why Prelude.lex returns a list, not Maybe?

--- Prelude/lex.hs
+++ Prelude/lex.hs
@@ -1,11 +1,20 @@
+{-# LANGUAGE ViewPatterns #-}
+
+import Test.QuickCheck (quickCheck)
+
+lexTest :: String -> Bool
+lexTest = (<=1) . length . lex
+
 main :: IO ()
 main = do
   print $ lex "abc123 abc 123"
   print $ lex "abc 123"
   print $ lex "123"
   print $ lex ""
+  quickCheck lexTest
 -- {output}
 -- [("abc123"," abc 123")]
 -- [("abc"," 123")]
 -- [("123","")]
 -- [("","")]
+-- +++ OK, passed 100 tests.
