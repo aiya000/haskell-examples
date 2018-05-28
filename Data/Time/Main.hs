e3e3cf999fe6c540feec6d24049a5372ff5aae75
aiya000 <aiya000.develop@gmail.com>
2018-05-28 22:23:41 +0900

    :+1: Learn how to get diff times

--- Data/Time/Main.hs
+++ Data/Time/Main.hs
@@ -1,10 +1,18 @@
-import Data.Time.Clock (UTCTime(..), DiffTime, getCurrentTime)
+import Control.Concurrent (threadDelay)
 import Data.Time.Calendar (Day)
+import Data.Time.Clock (UTCTime(..), DiffTime, getCurrentTime, diffUTCTime)
 
 main :: IO ()
 main = do
-  x <- (getCurrentTime :: IO UTCTime)
+  x <- getCurrentTime :: IO UTCTime
   let day  = utctDay x     :: Day
   let time = utctDayTime x :: DiffTime
   putStrLn $ "today is " ++ show day
   putStrLn $ "now is "   ++ show time
+  threadDelay $ 2 * 1000 * 1000
+  y <- getCurrentTime
+  print $ diffUTCTime y x
+-- {output}
+-- today is 2018-05-28
+-- now is 48191.858588564s
+-- 2.002086728s
