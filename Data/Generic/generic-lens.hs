322641cae0b421f1b08c3dcb950288ab7c5934ba
aiya000 <aiya000.develop@gmail.com>
2018-11-16 23:07:27 +0900

    :+1: Learn generic-lens's the constraints and the any

--- Data/Generic/generic-lens.hs
+++ Data/Generic/generic-lens.hs
@@ -7,8 +7,9 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Control.Lens ((^.), (^?), (.~), (&), (#))
-import Data.Generics.Product (field, position, typed, super, upcast, constraints')
-import Data.Generics.Sum (_Ctor, _Typed)
+import Data.Functor.Identity (Identity)
+import Data.Generics.Product (field, position, typed, super, upcast, constraints', the)
+import Data.Generics.Sum (_Ctor, _Typed, _As)
 import GHC.Generics (Generic)
 
 data Sugar = Sugar
@@ -35,12 +36,12 @@ data Point = Point Int Int
 point :: Point
 point = Point 100 200
 
-data Skeleton = Skeleton
-  { bones :: String
+newtype Skeleton = Skeleton
+  { skeleton :: String
   } deriving (Generic, Show)
 
 data Sans = Sans
-  { bones :: String
+  { skeleton :: String
   , lazy :: Int
   } deriving (Generic, Show)
 
@@ -49,9 +50,10 @@ sans = Sans ";E" 1
 
 lenses :: IO ()
 lenses = do
+  -- Fields
   print $ sugar ^. field @"sweet"
-  print $ sugar & field @"moon" .~ 20
-  -- Product types
+  print $ sugar & field @"moon" .~ 10003
+  -- Sum types
   print $ asgore ^. field @"kind"
   -- Not able to
   --print $ toriel ^. field @"kind"
@@ -72,22 +74,51 @@ lenses = do
   print $ sans ^. super @Skeleton
   print (upcast sans :: Skeleton)
   -- Constraints
-  -- print $ constraints' @Num twice point
+  print $ constraints' @Num (twice @Identity) point
+  -- Fields, positional, or typical
+  print $ sugar ^. the @String
+  print $ asgore ^. the @1
+  print $ sans ^. the @"skeleton" -- I'm Sans. Sans the skeleton ;E
   where
-    twice :: Num a => a -> a
-    twice = (* 2)
+    twice :: (Applicative f, Num a) => a -> f a
+    twice = pure . (*2)
 
 prisms :: IO ()
 prisms = do
   -- Constructors
+  print $ sugar ^? _Ctor @"Sugar"
   print $ toriel ^? _Ctor @"Toriel"
   print $ asgore ^? _Ctor @"Toriel"
-  print $ sugar ^? _Ctor @"Sugar"
   print (_Ctor @"Asgore" # ":O" :: Fluffy)
   -- Typical
   print $ toriel ^? _Typed @String
   -- Not able to
   --print $ toriel ^? _Typed @()
+  -- Constructors, or typical
+  print $ toriel ^? _As @"Toriel"
+  print $ asgore ^? _As @String
 
 main :: IO ()
 main = lenses >> prisms
+-- {output}
+-- "me"
+-- Sugar {sweet = "me", moon = 10003}
+-- ":)"
+-- "me"
+-- 1000
+-- "yours"
+-- "me"
+-- ":)"
+-- Skeleton {skeleton = ";E"}
+-- Skeleton {skeleton = ";E"}
+-- Identity (Point 200 400)
+-- "me"
+-- ":)"
+-- ";E"
+-- Just ("me",1000)
+-- Just (":D",())
+-- Nothing
+-- Asgore {kind = ":O"}
+-- Nothing
+-- Just (":D",())
+-- Just ":)"
