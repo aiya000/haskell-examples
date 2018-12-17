7ddcff80fef047df2729802b7db47cf01cc8d53f
aiya000 <aiya000.develop@gmail.com>
2018-12-17 22:16:57 +0900

    :recycle: Refactor

--- Text/Megaparsec/Main.hs
+++ Text/Megaparsec/Main.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE TypeApplications #-}
 
 import Data.Void (Void)
-import Text.Megaparsec (parseTest, parse)
+import Text.Megaparsec (Parsec, parseTest, parse)
 import qualified Control.Applicative as P
 import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as P hiding (space)
 
 main :: IO ()
 main = do
-  (parseTest @Void @String) parser "hi 10"
+  parseTest @Void @String parser "hi 10"
   print $ parse parser "no-name" "hi 10"
   print $ parse parser "no-name" "10"
   where
+    parser :: Parsec Void String (String, Int)
     parser = do
       hi <- P.many P.alphaNumChar
       P.space
--- Text/Regex/Posix/Main.hs
+++ Text/Regex/Posix/Main.hs
@@ -2,6 +2,6 @@ import Text.Regex.Posix
 
 main :: IO ()
 main = do
-  print $ ("abc" =~ "b" :: Bool)
-  print $ ("abc" =~ "b" :: Int)
-  print $ ("abc" =~ "b" :: String)
+  print ("abc" =~ "b" :: Bool)
+  print ("abc" =~ "b" :: Int)
+  print ("abc" =~ "b" :: String)
